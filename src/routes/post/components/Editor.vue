a9f6c90cd313cff9c4bcc4157a8dea4d7ca7a1d8
Yoonhwan Jung <unani92@naver.com>
2023-08-03 00:16:41 +0900

    저장은 잘 되는데 수정이 안됨

--- src/routes/post/components/Editor.vue
+++ src/routes/post/components/Editor.vue
@@ -6,6 +6,7 @@
 </template>
 
 <script>
+import youtubeIcon from '../../../assets/images/icons/youtubeIcon.png'
 import { Editor } from '@toast-ui/editor'
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax'
 import postService from '@/services/post'
@@ -28,7 +29,10 @@ export default {
     methods: {
         save() {
             const data = this.editor.getHTML()
-            this.$emit('save', data)
+            // const data = this.editor.getMarkdown()
+            // console.log(this.editor)
+            console.log(data)
+            // this.$emit('save', data)
         },
         preparePayload(payload) {
             const formData = new FormData()
@@ -40,12 +44,50 @@ export default {
         },
     },
     mounted() {
-        // this.editor = new Editor.factory({
-        //     el: document.getElementById('editor'),
-        //     viewer: true,
-        //     initialValue: ''
-        // })
-        this.editor = new Editor({
+        const myCustomEl = document.createElement('span')
+        myCustomEl.style = 'cursor: pointer;'
+
+        const icon = document.createElement('img')
+        icon.setAttribute('src', youtubeIcon)
+        icon.setAttribute('width', '32')
+        myCustomEl.appendChild(icon)
+
+        // 팝업 바디 생성
+        const container = document.createElement('div')
+        const description = document.createElement('p')
+
+        description.textContent = 'Youtube 주소를 입력하고 Enter를 누르세요!'
+
+        const urlInput = document.createElement('input')
+        urlInput.style.width = '100%'
+
+        // 팝업 input 창에 내용 입력 시 호출됨
+        urlInput.addEventListener('keyup', e => {
+            // 엔터를 누르면, 입력값이 Youtube 주소인지 정규식으로 검사
+            if (e.key === 'Enter') {
+                if (
+                    /https:\/\/youtu.be\/.{11,}/.test(e.target.value) ||
+                    /https:\/\/www.youtube.com\/watch\?v=.{11,}/.test(e.target.value)
+                ) {
+                    const str =
+                        '<iframe style="overflow:hidden" src="https://www.youtube-nocookie.com/embed/' +
+                        e.target.value.slice(-11) +
+                        '" title="YouTube video player" frameborder="0" allow="clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen; background;" allowfullscreen></iframe>'
+
+                    // 마크다운 모드에서 iframe 태그 삽입 후, 팝업을 닫고 위지윅 모드로 변환
+                    this.editor.changeMode('markdown')
+                    this.editor.insertText(str)
+                    this.editor.eventEmitter.emit('closePopup')
+                    this.editor.changeMode('wysiwyg')
+                    e.target.value = ''
+                }
+            }
+        })
+
+        container.appendChild(description)
+        container.appendChild(urlInput)
+
+        this.editor = new Editor.factory({
             el: document.getElementById('editor'),
             initialEditType: 'wysiwyg',
             initialValue: this.initialValue || '',
@@ -54,6 +96,17 @@ export default {
             toolbarItems: [
                 ['heading', 'bold', 'italic', 'strike'],
                 ['image', 'link'],
+                [
+                    {
+                        name: 'Youtube',
+                        tooltip: 'Youtube',
+                        el: myCustomEl,
+                        popup: {
+                            body: container,
+                            style: { width: 'auto', left: '50% !important' },
+                        },
+                    },
+                ],
             ],
             plugins: [colorSyntax],
             hooks: {
@@ -79,7 +132,26 @@ export default {
                     callback(imgUrl, 'img')
                 },
             },
+            customHTMLRenderer: {
+                htmlBlock: {
+                    iframe(node, context) {
+                        console.log('iframe', node.literal)
+                        console.log(2, context)
+                        return [
+                            {
+                                type: 'openTag',
+                                tagName: 'iframe',
+                                outerNewLine: true,
+                                attributes: node.attrs,
+                            },
+                            // { type: 'html', content: node.childrenHTML },
+                            { type: 'closeTag', tagName: 'iframe', outerNewLine: true },
+                        ]
+                    },
+                },
+            },
         })
+        setTimeout(() => this.editor.changeMode('wysiwyg'), 100)
     },
 }
 </script>
