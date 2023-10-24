e090d4e521bf467ca70b1ff82f7432381bf7a5a8
Yoonhwan Jung <53211781+unani92@users.noreply.github.com>
2023-10-25 00:16:08 +0900

    rm unused

--- src/modules/hackle.js
+++
@@ -1,7 +0,0 @@
-// 핵클 설정
-import * as Hackle from '@hackler/js-client-sdk'
-
-const key =
-    process.env.NODE_ENV === 'production' ? 'oUvxyrB526B7hx7T89wBTcyD2yRj2TE8' : 'KuHgG8O05SOE1tnnt4jmKtYWtQ6daRGS'
-
-export const hackleClient = Hackle.createInstance(key)
